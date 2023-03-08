8dbb4c3766e21e22c837b13b3f17135b240f30c3
KishiTheMechanic <s.kishi@elsoul.nl>
2023-03-08 14:45:17 +0100

    Discord

--- src/config/site.ts
+++ src/config/site.ts
@@ -5,12 +5,14 @@ const siteConfig = {
   sitenameEN: 'Skeet Web Media Template',
   keywordsJA: 'Next.js, SSG, テンプレート, メディア, SEO, 多言語対応',
   keywordsEN: 'Next.js, SSG, Template, Media, SEO, i18n translation',
-  descriptionJA: 'Next.jsのスターターキット。SEO対応、多言語対応、SSG。オープンソースで開発できます。',
-  descriptionEN: 'Next.js Starter kit. SEO compatible, i18n translation, SSG. You can develop the media as open source.',
+  descriptionJA:
+    'Next.jsのスターターキット。SEO対応、多言語対応、SSG。オープンソースで開発できます。',
+  descriptionEN:
+    'Next.js Starter kit. SEO compatible, i18n translation, SSG. You can develop the media as open source.',
   twitterAccount: '@ELSOUL_LABO2',
   instagramAccount: 'elsoul_labo',
   githubAccount: 'elsoul',
-  discordInvitationLink: 'https://discord.gg/GmHYfyRamx',
+  discordInvitationLink: 'https://discord.gg/H2HeqRq54J',
 }
 
 export default siteConfig
